066763fa62cf9b3d3d86b2263906cc58abf692cc
Zach Seaman <znseaman@gmail.com>
2019-07-19 18:03:13 -0700

    Remove commented out code

--- client/src/components/Listings/Listings.js
+++ client/src/components/Listings/Listings.js
@@ -4,7 +4,7 @@ import Listing from "./Listing/Listing";
 import useListings from "../../hooks/useListings";
 
 const Listings = () => {
-	const [listings] = useListings([]);
+	const [listings] = useListings();
 
 	const all = listings.map(listing => {
 		// use 1st image from the imageUrls
@@ -14,7 +14,6 @@ const Listings = () => {
 				key={listing.id}
 				title={listing.title}
 				url={listing.url}
-				// date={listing.date}
 				description={listing.description}
 				lat={listing.latitude}
 				lng={listing.longitude}
