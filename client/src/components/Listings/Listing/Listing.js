a855aadaf7ab391a48863813bc7ce3a1da74b86e
Zach Seaman <znseaman@gmail.com>
2019-07-22 14:08:39 -0700

    Add postedAt humanized date for cards

--- client/src/components/Listings/Listing/Listing.js
+++ client/src/components/Listings/Listing/Listing.js
@@ -1,32 +1,50 @@
 import React from "react";
+import { formatDistanceStrict } from "date-fns";
 import classes from "./Listing.module.css";
 
 export default function Listing(props) {
+	const { id, title, description, postedAt, clicked } = props;
+
+	// use 1st image from the imageUrls
+	const [img] = props.imageUrls.split(",");
+
+	const humanized = formatDistanceStrict(new Date(postedAt), Date.now());
+
 	return (
 		<article className={classes.Listing}>
 			<header
 				style={{
-					padding: "2rem 1rem",
+					padding: "1rem 1rem",
+					paddingTop: "3rem",
 					textOverflow: "ellipsis",
 					whiteSpace: "nowrap"
 				}}
 			>
-				<p className={classes["Listing-title"]}>{props.title}</p>
+				<p className={classes["Listing-title"]}>{title}</p>
 			</header>
 			<div>
-				<img src={props.img} alt="" />
+				<img src={img} alt="" />
 			</div>
-			<div style={{ padding: "1rem" }}>
+			<div style={{ padding: ".5rem 1rem" }}>
 				<p
 					className={classes["Listing-description"]}
 					style={{ "-webkit-box-orient": "vertical" }}
 				>
-					{props.description}
+					{description}
 				</p>
 			</div>
-			<div>
+			<div style={{ padding: "1rem 1rem" }}>
+				<button>Details</button>
 				<button>Favorite</button>
 			</div>
+			<p
+				style={{
+					position: "absolute",
+					fontSize: ".65rem",
+					top: 0,
+					padding: "1rem"
+				}}
+			>{`${humanized}`}</p>
 			<a
 				href=""
 				aria-label="Delete Listing"
@@ -38,7 +56,7 @@ export default function Listing(props) {
 					right: "0",
 					padding: "0 .25rem"
 				}}
-				onClick={() => props.clicked(props.id)}
+				onClick={() => clicked(id)}
 			>
 				&times;
 			</a>
--- client/src/components/Listings/Listings.js
+++ client/src/components/Listings/Listings.js
@@ -7,18 +7,10 @@ const Listings = () => {
 	const [listings, deleteListingHandler] = useListings();
 
 	const all = listings.map(listing => {
-		// use 1st image from the imageUrls
-		const [img] = listing.imageUrls.split(",");
 		return (
 			<Listing
 				key={listing.id}
-				id={listing.id}
-				title={listing.title}
-				url={listing.url}
-				description={listing.description}
-				lat={listing.latitude}
-				lng={listing.longitude}
-				img={img}
+				{...listing}
 				clicked={deleteListingHandler}
 			/>
 		);
