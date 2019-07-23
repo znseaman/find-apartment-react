c8be924d757b65e9dd8d47a32c7b85b019fed57a
Zach Seaman <znseaman@gmail.com>
2019-07-22 19:09:23 -0700

    Move fetching listings into crons directory

---
+++ server/crons/fetchFromCraigslist.js
@@ -0,0 +1,79 @@
+const Listing = require("../models/listing");
+
+const fetchDataFromCraigslist = async () => {
+	console.log("Getting data from craigslist...");
+	// TODO: instead of getting these from data, have them come from the DB
+	const PolygonLookup = require("polygon-lookup");
+	const featureCollection = require("../data/polygons.json");
+	const lookup = new PolygonLookup(featureCollection);
+
+	const craigslist = require("node-craigslist");
+	const userPreferences = require("../utils/userPreferences");
+	const { baseHost, city } = userPreferences;
+	let client = new craigslist.Client({
+		baseHost,
+		city
+	});
+	const listings = await client.search(userPreferences, "");
+	for (const listing of listings) {
+		const details = await client.details(listing);
+
+		const { mapUrl } = details;
+		if (!mapUrl) {
+			break;
+		}
+
+		// extract the coordinates from mapUrl
+		const hasCoords = details.mapUrl.match(
+			/@(-?\d+\.\d+),(-?\d+\.\d+),(\d+\.?\d?)+z/g
+		);
+
+		if (hasCoords) {
+			let coordinates = hasCoords[0]
+				.replace("@", "")
+				.replace("z", "")
+				.split(",");
+			var [latitude, longitude, zoom] = coordinates;
+
+			const { description, pid, title, url, images, postedAt } = details;
+
+			const regex = new RegExp(`no pets`, "i");
+
+			// has images & there's no mention of no pets
+			if (images && !regex.test(description)) {
+				// just combine them
+				const imageUrls = images.join(",");
+
+				// is lat,lng is within one of the polygons
+				var poly = lookup.search(longitude, latitude);
+				if (poly) {
+					const { name: polygon_name } = poly.properties;
+					const point = {
+						type: "Point",
+						coordinates: [longitude, latitude],
+						crs: {
+							type: "name",
+							properties: { name: "EPSG:4326" }
+						}
+					};
+					Listing.create({
+						description,
+						pid,
+						title,
+						url,
+						imageUrls,
+						latitude,
+						longitude,
+						point,
+						zoom,
+						postedAt,
+						polygon_name,
+						userId: 1
+					});
+				}
+			}
+		}
+	}
+};
+
+module.exports = fetchDataFromCraigslist;
