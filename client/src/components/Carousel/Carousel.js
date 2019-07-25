a2cde8cd12d52ef65a90c4537bb1935d6406d387
Zach Seaman <znseaman@gmail.com>
2019-07-25 11:27:03 -0700

    Fix error by adding key to img tags in carousel

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import v4 from "uuid/v4";
 
 const carousel = ({ imageUrls, showThumbs = false, dynamicHeight = false }) => {
 	return (
 		<Carousel showThumbs={showThumbs} dynamicHeight={dynamicHeight}>
 			{imageUrls.map((src, i) => (
-				<img src={src} alt={`Photo ` + (i + 1)} />
+				<img key={v4()} src={src} alt={`Photo ` + (i + 1)} />
 			))}
 		</Carousel>
 	);
