ca746a6c03b4eeade7e5a19ed20c201592d115a5
Zach Seaman <znseaman@gmail.com>
2019-08-01 08:54:14 -0700

    Remove unused import

--- client/src/components/SimpleMap/SimpleMap.js
+++ client/src/components/SimpleMap/SimpleMap.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 /* Boilerplate for Leaflet to work in React */
