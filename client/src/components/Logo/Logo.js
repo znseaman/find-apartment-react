0be1b92efa261f1ae33e8a55426f8dad43be9c2b
Zach Seaman <znseaman@gmail.com>
2019-10-15 17:46:33 -0700

    Improve logo using fontawesome React comp

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,12 +1,12 @@
 import React from "react";
-
-import findApartmentSVG from "../../assets/images/building-regular.svg";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./Logo.module.css";
 
 const Logo = ({ height }) => {
 	return (
 		<div className={classes.Logo} style={{ height }}>
-			<img src={findApartmentSVG} alt="Find Apartment Logo"></img>
+			<FontAwesomeIcon icon={faBuilding}></FontAwesomeIcon> {"Find Apartment"}
 		</div>
 	);
 };
