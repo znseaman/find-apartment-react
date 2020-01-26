8aee633b6146a506c8c49b104744dda944e322ca
Zach Seaman <znseaman@gmail.com>
2020-01-26 02:43:32 -0800

    feat(ux): make Logo clickable

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./Logo.module.css";
@@ -6,7 +7,8 @@ import classes from "./Logo.module.css";
 const Logo = ({ height }) => {
 	return (
 		<div className={classes.Logo} style={{ height }}>
-			<FontAwesomeIcon icon={faBuilding}></FontAwesomeIcon> {"Find Apartment"}
+			<NavLink exact to="/">
+				<FontAwesomeIcon icon={faBuilding}></FontAwesomeIcon> {"Find Apartment"}</NavLink>
 		</div>
 	);
 };
