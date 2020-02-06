beef885bf54126d2067b3cff1a0fecb1ada939ec
Zach Seaman <znseaman@gmail.com>
2020-02-06 03:11:24 -0800

    test: check if pages render based on route - App

--- client/src/containers/App/App.test.js
+++ client/src/containers/App/App.test.js
@@ -17,3 +17,69 @@ it('renders without crashing', () => {
   </Provider>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
+
+it('renders /login without crashing', () => {
+  const pathname = '/login'
+  history.push(pathname)
+
+  const div = document.createElement('div')
+  ReactDOM.render(<Provider store={store}>
+    <Router history={history}>
+      <App auth={auth} history={history}></App>
+    </Router>
+  </Provider>, div)
+
+  expect(history.location.pathname).toBe(pathname)
+
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it('renders /signup without crashing', () => {
+  const pathname = '/signup'
+  history.push(pathname)
+
+  const div = document.createElement('div')
+  ReactDOM.render(<Provider store={store}>
+    <Router history={history}>
+      <App auth={auth} history={history}></App>
+    </Router>
+  </Provider>, div)
+
+  expect(history.location.pathname).toBe(pathname)
+
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it('will redirect from / to /login when not logged in', () => {
+  const pathname = '/'
+  history.push(pathname)
+
+  const div = document.createElement('div')
+  ReactDOM.render(<Provider store={store}>
+    <Router history={history}>
+      <App auth={auth} history={history}></App>
+    </Router>
+  </Provider>, div)
+
+  expect(history.location.pathname).toBe('/login')
+
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it('will not redirect from / when logged in', () => {
+  const pathname = '/'
+  history.push(pathname)
+
+  auth.loggedIn = true;
+
+  const div = document.createElement('div')
+  ReactDOM.render(<Provider store={store}>
+    <Router history={history}>
+      <App auth={auth} history={history}></App>
+    </Router>
+  </Provider>, div)
+
+  expect(history.location.pathname).toBe(pathname)
+
+  ReactDOM.unmountComponentAtNode(div)
+})
