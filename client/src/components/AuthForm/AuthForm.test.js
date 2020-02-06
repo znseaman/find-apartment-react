da0e0f2d99e8b88fb13d32be8429fbdc06aa33c7
Zach Seaman <znseaman@gmail.com>
2020-02-06 12:08:58 -0800

    test: improve AuthForm with @testing-library/react

--- client/src/components/AuthForm/AuthForm.test.js
+++ client/src/components/AuthForm/AuthForm.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
-import { getQueriesForElement } from '@testing-library/dom'
+import { render } from '@testing-library/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router-dom'
@@ -17,46 +17,42 @@ describe('AuthForm', () => {
   it('should render for /login route', () => {
     const pathname = '/login'
     history.push(pathname)
-    const div = document.createElement('div')
-    ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
+
+    const { container, getByLabelText } = render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>)
 
     expect(history.location.pathname).toBe(pathname)
 
     // Inputs
-    const { getByLabelText } = getQueriesForElement(div);
-    const inputEmail = getByLabelText(/email address/i);
+    const inputEmail = getByLabelText(/email address/i)
     expect(inputEmail).toHaveAttribute('type', 'email')
     expect(inputEmail).toHaveAttribute('required')
-
-    const inputPassword = getByLabelText(/password/i);
+    const inputPassword = getByLabelText(/password/i)
     expect(inputPassword).toHaveAttribute('type', 'password')
     expect(inputPassword).toHaveAttribute('required')
 
     // Submit Button
-    expect(div.querySelector('button')).toHaveAttribute('type', 'submit')
-    expect(div.querySelector('button')).toHaveTextContent('Log In')
+    expect(container.querySelector('button')).toHaveAttribute('type', 'submit')
+    expect(container.querySelector('button')).toHaveTextContent('Log In')
   })
 
   it('should render for /signup route', () => {
     const pathname = '/signup'
     history.push(pathname)
-    const div = document.createElement('div')
-    ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
+    const { container, getByLabelText } = render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>)
 
     expect(history.location.pathname).toBe(pathname)
 
     // Inputs
-    const { getByLabelText } = getQueriesForElement(div);
-    const inputEmail = getByLabelText(/email address/i);
+    const inputEmail = getByLabelText(/email address/i)
     expect(inputEmail).toHaveAttribute('type', 'email')
     expect(inputEmail).toHaveAttribute('required')
 
-    const inputPassword = getByLabelText(/password/i);
+    const inputPassword = getByLabelText(/password/i)
     expect(inputPassword).toHaveAttribute('type', 'password')
     expect(inputPassword).toHaveAttribute('required')
 
     // Submit Button
-    expect(div.querySelector('button')).toHaveAttribute('type', 'submit')
-    expect(div.querySelector('button')).toHaveTextContent('Sign Up')
+    expect(container.querySelector('button')).toHaveAttribute('type', 'submit')
+    expect(container.querySelector('button')).toHaveTextContent('Sign Up')
   })
 })
