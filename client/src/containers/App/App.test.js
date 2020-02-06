50e36fc2533bf8f670e88ccb49f36dc7dca0daa6
Zach Seaman <znseaman@gmail.com>
2020-02-06 01:34:58 -0800

    test: make App render without crashing

--- client/src/containers/App/App.test.js
+++ client/src/containers/App/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Router } from 'react-router-dom'
+import history from '../../utils/history'
+import { auth } from '../../components/Auth/Auth'
+
+import { Provider } from 'react-redux'
+import store from '../../redux/store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Provider store={store}>
+    <Router history={history}>
+      <App auth={auth} history={history}></App>
+    </Router>
+  </Provider>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
