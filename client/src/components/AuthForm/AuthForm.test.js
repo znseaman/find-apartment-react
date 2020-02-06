6cda2741d8f9625bd67dc47823c852c5ea165df6
Zach Seaman <znseaman@gmail.com>
2020-02-06 11:23:17 -0800

    test: improve tests for AuthForm

--- client/src/components/AuthForm/AuthForm.test.js
+++ client/src/components/AuthForm/AuthForm.test.js
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router-dom'
@@ -5,28 +6,54 @@ import AuthForm from './AuthForm'
 import history from '../../utils/history'
 import { auth } from '../../components/Auth/Auth'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
+describe('AuthForm', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 
-it('should render for /login route', () => {
-  const pathname = '/login'
-  history.push(pathname)
-  const div = document.createElement('div')
-  ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
+  it('should render for /login route', () => {
+    const pathname = '/login'
+    history.push(pathname)
+    const div = document.createElement('div')
+    ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
 
-  expect(history.location.pathname).toBe(pathname)
-  expect(div.querySelector('button').textContent).toBe('Log In')
-})
+    expect(history.location.pathname).toBe(pathname)
 
-it('should render for /signup route', () => {
-  const pathname = '/signup'
-  history.push(pathname)
-  const div = document.createElement('div')
-  ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
+    // Inputs
+    expect(div.querySelector('input#form-email')).toHaveAttribute('type', 'email')
+    expect(div.querySelector('input#form-email')).toHaveAttribute('required')
+    expect(div.querySelector('label[for="form-email"]')).toHaveTextContent('Email')
 
-  expect(history.location.pathname).toBe(pathname)
-  expect(div.querySelector('button').textContent).toBe('Sign Up')
+    expect(div.querySelector('input#form-password')).toHaveAttribute('type', 'password')
+    expect(div.querySelector('input#form-password')).toHaveAttribute('required')
+    expect(div.querySelector('label[for="form-password"]')).toHaveTextContent('Password')
+
+    // Submit Button
+    expect(div.querySelector('button')).toHaveAttribute('type', 'submit')
+    expect(div.querySelector('button')).toHaveTextContent('Log In')
+  })
+
+  it('should render for /signup route', () => {
+    const pathname = '/signup'
+    history.push(pathname)
+    const div = document.createElement('div')
+    ReactDOM.render(<Router history={history}><AuthForm {...history} auth={auth} /></Router>, div)
+
+    expect(history.location.pathname).toBe(pathname)
+
+    // Inputs
+    expect(div.querySelector('input#form-email')).toHaveAttribute('type', 'email')
+    expect(div.querySelector('input#form-email')).toHaveAttribute('required')
+    expect(div.querySelector('label[for="form-email"]')).toHaveTextContent('Email')
+
+    expect(div.querySelector('input#form-password')).toHaveAttribute('type', 'password')
+    expect(div.querySelector('input#form-password')).toHaveAttribute('required')
+    expect(div.querySelector('label[for="form-password"]')).toHaveTextContent('Password')
+
+    // Submit Button
+    expect(div.querySelector('button')).toHaveAttribute('type', 'submit')
+    expect(div.querySelector('button')).toHaveTextContent('Sign Up')
+  })
 })
